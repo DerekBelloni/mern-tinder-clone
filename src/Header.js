110128bcccf006de66669c22f740212a61b09ff5
Derek <derekbelloni10@gmail.com>
2022-06-08 12:46:06 -0600

    header component stylings

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
 import { IconButton } from '@mui/material';
+import tinderLogo from './images/tinder-logo.png';
+import ForumIcon from '@mui/icons-material/Forum';
 
 const Header = () => {
   return (
     <>
       <div className="header">
-        <h1>header component</h1>
-        <PersonIcon fontSize="large" className="header_icon" />
+        <IconButton>
+          <PersonIcon fontSize="large" className="header_person" />
+        </IconButton>
+        <img src={tinderLogo} className="header_logo" />
+        <IconButton>
+          <ForumIcon fontSize="large" className="header_forum" />
+        </IconButton>
       </div>
     </>
   );
