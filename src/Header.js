3b08c7fde09d9581e4ebd20e96c91cc4e80c90ad
Derek <derekbelloni10@gmail.com>
2022-06-08 11:58:39 -0600

    icon written into header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+import { IconButton } from '@mui/material';
 
 const Header = () => {
   return (
-    <div className="header">
-
-    </div>
+    <>
+      <div className="header">
+        <h1>header component</h1>
+        <PersonIcon fontSize="large" className="header_icon" />
+      </div>
+    </>
   );
 }
 
