798f727b52273354f7da484e16499f59a46d1339
Derek <derekbelloni10@gmail.com>
2022-06-08 11:58:28 -0600

    material design installed, icon written into header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 
+
 function App() {
   return (
     <div className="app">
-      <h1>Tinder Clone</h1>
+
       <Header />
       {/* tinder card component */}
       {/* swipe component */}
